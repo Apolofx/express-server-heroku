c873306ea891ffbe4868178a4af9071c855614e6
nacho <nacho.conso@gmail.com>
2020-06-06 21:57:10 -0300

    changed body to params

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const cors = require("cors");
 
 app.use(cors({ origin: true }));
 app.use(express.json());
-app.get("/", (req, res) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${process.env.API_KEY}`;
+app.get("/:city", (req, res) => {
+  // const city = req.params.city.split("%20").join(" ");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}`;
   return axios
     .get(url)
     .then((response) => res.status(200).send(response.data))
