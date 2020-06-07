d56f8a0ea7de8c7f1022f8c7fe39615ca087854b
nacho <nacho.conso@gmail.com>
2020-06-06 22:44:11 -0300

    changed name

--- build/server.js
+++ build/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.get("/:city", (req, res) => {
   // const city = req.params.city.split("%20").join(" ");
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}`;
-  return axios.get(url).then(response => res.status(200).send(response.data)).catch(error => res.status(500).send(error));
+  return axios.get(url).then(response => res.status(200).send(response.data.weather)).catch(error => res.status(500).send(error));
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.get("/:city", (req, res) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}`;
   return axios
     .get(url)
-    .then((response) => res.status(200).send(response.data))
+    .then((response) => res.status(200).send(response.data.weather))
     .catch((error) => res.status(500).send(error));
 });
 
